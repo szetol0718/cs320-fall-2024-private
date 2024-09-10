9896cbcee459e0f8ce7d45b096f3e03f9c06b87c
Louis Szeto <louisszeto@crc-dot1x-nat-10-239-254-226.bu.edu>
2024-09-10 19:13:25 -0400

    add

--- assigns/assign01/lib/assign01_04.ml
+++ assigns/assign01/lib/assign01_04.ml
@@ -1,12 +1,1 @@
-open Assign01_02
-open Assign01_03
-
-let to_string s = 
-  let rec decode s i acc =
-    if s = 1 then List.rev acc
-    else 
-      let ex = nth s i in 
-      decode (s/(nth_prime i) ** ex) (i+1) ( ex:: acc)
-    in
-    let elements = decode s 0 [] in
-    "[" ^ (Sting.concat ";" (List.map string_of_int elements)) ^ "]" 
+let to_string = string_of_int
